50a9ab516af8a87aa99908104ece494f3cf73047
michg <michael_gall@gmx.de>
2022-08-16 14:25:14 +0200

    filter out xori operation with immediate zero

--- rv64/emit.c
+++ rv64/emit.c
@@ -415,6 +415,10 @@ emitins(Ins *i, Fn *fn, Blk *b, int id0, FILE *f)
 		if(i->to.val != i->arg[0].val)
 			emitf("mv %=, %0", i, fn, f);
 		return;
+	case Oxor:
+	   if(i->to.val == i->arg[0].val && rtype(i->arg[1]) == RCon && fn->con[i->arg[1].val].bits.i == 0)
+	       return;
+	   goto Table;
 	case Orbeq:
 		bname = beq;
 		break;
